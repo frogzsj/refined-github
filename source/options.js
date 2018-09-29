ab84ab35069da6db16fa2b11a6d2a2409cfb18aa
Franz Silva-Jetter <fsilva@box.com>
2018-09-29 13:54:47 -0700

    add enterprise support for vivaldi

--- source/options.js
+++ source/options.js
@@ -13,4 +13,30 @@ document.querySelector('[name="customCSS"]').addEventListener('keydown', event =
 	}
 });
 
+document.querySelector('#enable-on-this-domain-btn').addEventListener('click', event => {
+	const actionId = 'webext-domain-permission-toggle:add-permission';
+	const {name: extensionName} = chrome.runtime.getManifest();
+	let options = Object.assign({
+		title: `Enable ${extensionName} on this domain`,
+		reloadOnSuccess: `Do you want to reload this page to apply ${extensionName}?`
+	}, {});
+
+	chrome.tabs.query({'active': true}, function (tabs) {
+		const currentUrl = "" + tabs[0].url;
+		chrome.permissions.request({
+			origins: [
+				`${new URL(currentUrl).origin}/*`
+			]
+		}, granted => {
+			if (chrome.runtime.lastError) {
+				console.error(`Error: ${chrome.runtime.lastError}`, currentUrl);
+				alert(`Error: ${chrome.runtime.lastError.message}`);
+			} else if (granted && options.reloadOnSuccess && confirm(options.reloadOnSuccess)) {
+				chrome.tabs.reload(tabId);
+			}
+		});
+		console.error(currentUrl);
+	});
+});
+
 new OptionsSync().syncForm('#options-form');
